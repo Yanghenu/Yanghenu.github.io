15a6532cea59f2d3b040d7af6803a3dfc3f8c0cf
Yanghenu <67721696+Yanghenu@users.noreply.github.com>
2022-04-08 14:53:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -140,3 +140,19 @@ Matrix - Particles.js;
 SliderJS - Ettrics;
 Fonts - Google Fonts
 */
+
+/*timer*/
+timer = setInterval(function () {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  var strTime = hours + " : " + minutes + " : " + seconds + " " + ampm;
+  document.getElementById("clock").innerHTML = strTime;
+}
+, 1000);
